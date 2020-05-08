7e8a7445780c59aab93ffd7171be000c4d66c8ed
deannabosschert <deanna.bosschert@gmail.com>
2020-05-08 13:40:41 +0200

    second search

--- server.js
+++ server.js
@@ -40,10 +40,9 @@ io.on('connection', socket => {
     console.log(username_1)
     console.log(username_2)
     if (username_2 == "empty") {
-      console.log("wtf")
       getInfo_1(username_1, '', '')
     } else if (username_1 = "empty") {
-      // getInfo_2(username_2, '', '')
+      getInfo_2(username_2, '', '')
     }
   })
 
@@ -51,12 +50,24 @@ io.on('connection', socket => {
     socket.broadcast.emit('new_tweet_1', username_1, tweetObject)
   })
 
-  socket.on('new_follower', function(username_1, followers) {
-    socket.broadcast.emit('new_follower', username_1, followers)
+  socket.on('new_follower_1', function(username_1, followers) {
+    socket.broadcast.emit('new_follower_1', username_1, followers)
   })
 
   socket.on('refresh_tweet_1', function(username_1, latest_tweetObject) {
     getInfo_1(username_1, latest_tweetObject)
+  })
+
+  socket.on('new_tweet_2', function(username_2, tweetObject) {
+    socket.broadcast.emit('new_tweet_2', username_2, tweetObject)
+  })
+
+  socket.on('new_follower_2', function(username_2, followers) {
+    socket.broadcast.emit('new_follower_2', username_2, followers)
+  })
+
+  socket.on('refresh_tweet_2', function(username_2, latest_tweetObject) {
+    getInfo_2(username_2, latest_tweetObject)
   })
 })
 
@@ -82,14 +93,14 @@ function getInfo_1(username_1, latest_tweetObject) {
 
   getData
     .then(tweetObject => {
-      checkText(username_1, tweetObject, latest_tweetObject)
+      checkText_1(username_1, tweetObject, latest_tweetObject)
     })
     .catch(err => {
       console.log(err)
     })
 }
 
-function checkText(username_1, tweetObject, latest_tweetObject) {
+function checkText_1(username_1, tweetObject, latest_tweetObject) {
   const tweetText = tweetObject.text
   const latest_tweetText = latest_tweetObject.text
 
@@ -102,7 +113,7 @@ function checkText(username_1, tweetObject, latest_tweetObject) {
   }
 }
 
-function checkFollowers(username_1, followers, latest_followers) {
+function checkFollowers_1(username_1, followers, latest_followers) {
   if (followers == latest_followers) {
     console.log('same old fam')
     refreshTweet_1(username_1, followers)
@@ -112,12 +123,64 @@ function checkFollowers(username_1, followers, latest_followers) {
   }
 }
 
-
-
 function refreshTweet_1(username_1, tweetObject, latest_tweet_text) {
   getInfo_1(username_1, tweetObject, latest_tweet_text)
 }
 
+function getInfo_2(username_2, latest_tweetObject) {
+  const filterOptions = {
+    screen_name: username_2,
+    count: 1
+  }
+
+  const getData = new Promise((resolve) => {
+    twitterClient.get('statuses/user_timeline', filterOptions, function(err, data) {
+      const tweets = data.map(item => ({
+        text: item.text,
+        user_name: item.user.name,
+        user_screen_name: item.user.screen_name,
+        followers: item.user.followers_count
+      }))
+      const tweetObject = tweets[0]
+      resolve(tweetObject)
+    })
+  })
+
+  getData
+    .then(tweetObject => {
+      checkText_2(username_2, tweetObject, latest_tweetObject)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+function checkText_2(username_2, tweetObject, latest_tweetObject) {
+  const tweetText = tweetObject.text
+  const latest_tweetText = latest_tweetObject.text
+
+  if (tweetText == latest_tweetText) {
+    console.log('same old')
+    refreshTweet_2(username_2, tweetObject)
+  } else {
+    console.log('sunshine and rainbows: new tweet!')
+    io.emit("new_tweet_2", username_2, tweetObject)
+  }
+}
+
+function checkFollowers_2(username_2, followers, latest_followers) {
+  if (followers == latest_followers) {
+    console.log('same old fam')
+    refreshTweet_2(username_2, followers)
+  } else {
+    console.log('new follower count!')
+    io.emit("new_tweet_2", username_2, followers)
+  }
+}
+
+function refreshTweet_2(username_2, tweetObject, latest_tweet_text) {
+  getInfo_2(username_2, tweetObject, latest_tweet_text)
+}
 http.listen(port, () => {
   console.log('App listening on: ' + port)
 })
